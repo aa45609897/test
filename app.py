e7a3227835a3871821b9de73b185e878922b4d76
1 feng <2755017579@qq.com>
2025-03-31 17:26:18 +0800

    opoen

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ def home():
     return "<h1>Hello, Welcome to the Test Page!</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # 设置 host 为 0.0.0.0 使应用可以被外部访问
+    # 设置 port 为 5000 或其他您想要的端口
+    app.run(host='0.0.0.0', port=5000, debug=True)
